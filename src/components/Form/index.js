d6ec1213f20d078686e752d73dead8e6f6434b28
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-11-12 12:01:59 +0100

    ajout de messages : copie du tableau de messages dans le state

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,21 @@
+import { useDispatch } from 'react-redux';
 import './styles.scss';
 
-const Form = () => (
-  <form className="form">
-    <input type="text" placeholder="Saisir un message" className="form__message" />
-    <button type="submit" className="form__submit">&gt;</button>
-  </form>
-);
+const Form = () => {
+  const dispatch = useDispatch;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('formuluaire soumis, on veut ajouter un message');
+    dispatch({ type: 'ADD_NEW_MESSAGE_BIS' });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input type="text" placeholder="Saisir un message" className="form__message" />
+      <button type="submit" className="form__submit">&gt;</button>
+    </form>
+  );
+};
 
 export default Form;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -20,6 +20,29 @@ const initialState = {
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case 'ADD_NEW_MESSAGE_BIS': {
+      // on prépare le message à ajouter
+      const newMessage = {
+        id: 4,
+        author: 'Super Chat',
+        content: 'Test',
+      };
+
+      // on contruit une nouvelle liste de message à partir de la liste courante
+      // on respecte le principe d'immutabilité, on ne modifie jamais directement un tableau
+      // ou un objet dans le state.
+      const newMessages = [...state.messages];
+
+      // on ajoute le nouveau message à la nouvelle liste des messages
+      newMessages.push(newMessage);
+
+      // on retourn la nouvelle version de l'état
+      // dans la quelle la liste des messages est la liste que l'on vient de créer
+      return {
+        ...state,
+        messages: newMessages,
+      };
+    }
     default:
       return state;
   }
