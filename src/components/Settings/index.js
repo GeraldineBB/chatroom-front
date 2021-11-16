4b5ab21703e6ae2b9ffe60e86aa29dc13021d1fc
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-11-16 10:23:39 +0100

    Settings statique : CSS, animation

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,77 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import { setEmailContent, setPasswordContent, setPopupState } from '../../actions';
-
 import './styles.scss';
 
-const Settings = () => {
-  const dispatch = useDispatch();
-
-  const emailContent = useSelector(
-    (state) => state.emailContent,
-  );
-
-  const passwordContent = useSelector(
-    (state) => state.passwordContent,
-  );
-
-  const isPopUp = useSelector(
-    (state) => state.popUp,
-  );
-
-  const handleClickOnPopUp = () => {
-    dispatch(setPopupState());
-  };
-
-  const handleChangeEmail = (event) => {
-    dispatch(setEmailContent(event.target.value));
-  };
-
-  const handleChangePassword = (event) => {
-    dispatch(setPasswordContent(event.target.value));
-  };
-
-  const handleSubmit = () => {
-    dispatch(setEmailContent(emailContent));
-    dispatch(setPasswordContent(passwordContent));
-  };
-
-  return (
-    <div className="settings">
-      <button
-        type="submit"
-        className="settings__button"
-        onClick={handleClickOnPopUp}
-      >
-        X
-      </button>
-      <div className={isPopUp ? 'settings__ON' : 'settings__OFF'}>
-        <form className="settings__form">
-          <input
-            value={emailContent}
-            type="text"
-            placeholder="Email"
-            className="settings__form--email"
-            onChange={handleChangeEmail}
-          />
-          <input
-            value={passwordContent}
-            type="text"
-            placeholder="Mot de passe"
-            className="settings__form--password"
-            onChange={handleChangePassword}
-          />
-          <button
-            type="submit"
-            className="settings__form--submit"
-            onSubmit={handleSubmit}
-          >
-            Envoyer
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
+const Settings = () => (
+  <div className="settings">
+    <button type="button" className="settings__open-close">+</button>
+    <form className="settings__form">
+      <input type="email" placeholder="E-mail" className="settings__field" />
+      <input type="password" placeholder="Password" className="settings__field" />
+      <button type="submit" className="settings__submit">Envoyer</button>
+    </form>
+  </div>
+);
 
 export default Settings;
